0ce564da4b9cc857f7a353d8921a4712f6ed9c76
Prashant Singh <prashant22297@gmail.com>
2022-09-05 20:32:20 +0530

    Updates

---
+++ precache-manifest.b0cd00165147bfd3a0851f31d189654d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b5e3706d9992a9339e1",
+    "url": "/Covid19-tracker/static/css/main.4e76986e.chunk.css"
+  },
+  {
+    "revision": "9b5e3706d9992a9339e1",
+    "url": "/Covid19-tracker/static/js/main.9b5e3706.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/Covid19-tracker/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "47a563d46752977d5116",
+    "url": "/Covid19-tracker/static/css/2.97c53f4a.chunk.css"
+  },
+  {
+    "revision": "47a563d46752977d5116",
+    "url": "/Covid19-tracker/static/js/2.47a563d4.chunk.js"
+  },
+  {
+    "revision": "c4d3a35fad3852f6edee",
+    "url": "/Covid19-tracker/static/js/runtime~main.c4d3a35f.js"
+  },
+  {
+    "revision": "a17a5c52ea18b5c0dfa7b2751e9a0b1e",
+    "url": "/Covid19-tracker/index.html"
+  }
+];
